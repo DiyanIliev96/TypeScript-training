299afeefd72507da9b83861b00ba14482d585374
diyaniliev96 <diyan.iliev96@gmail.com>
2024-05-06 10:35:02 +0300

    TypeScript for loops and array training

---
+++ 03-for-loops-and-arrays/for-loop-and-array.ts
@@ -0,0 +1,11 @@
+const array: number[] = [1,2,9,7,5];
+
+for(let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
+// For of loop
+
+for (const number of array) {
+    console.log(number);
+}
