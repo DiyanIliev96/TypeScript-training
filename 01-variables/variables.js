42f819dcc12aea85a3f6bc7270c8fe3648cf4418
diyaniliev96 <diyan.iliev96@gmail.com>
2024-05-06 10:32:07 +0300

    TypeScript variables training

---
+++ 01-variables/variables.js
@@ -0,0 +1,8 @@
+var isAvailable = true;
+var age = 28;
+var firstName = 'Diyan';
+var lastName = "Iliev";
+console.log(isAvailable);
+console.log(age);
+console.log(firstName);
+console.log(lastName);
