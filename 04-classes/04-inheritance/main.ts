45e2afa63849da46dcd75dcd7f5d9e1ef209dbe5
diyaniliev96 <diyan.iliev96@gmail.com>
2024-05-09 16:49:59 +0300

    TypeScript adding Animals to array training

--- 04-classes/04-inheritance/main.ts
+++ 04-classes/04-inheritance/main.ts
@@ -1,3 +1,4 @@
+import { Animal } from "./animal";
 import { Dog } from "./dog";
 import { SuperDog } from "./super-dog";
 
@@ -5,7 +6,13 @@ import { SuperDog } from "./super-dog";
 let dog: Dog = new Dog('pesho',15)
 
 let superDog: SuperDog = new SuperDog('Gosho',15,'SUPER BAW');
+
+const animalsArray: Animal[] = [];
+animalsArray.push(dog);
+animalsArray.push(superDog);
 console.log(dog.animalInfo());
 console.log(dog.makeSound());
 console.log(superDog.makeSound());
+console.log(animalsArray.length);
+
 
