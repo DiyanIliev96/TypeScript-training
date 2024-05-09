91cfdb6be22434a6c1fbcfc42173581314a9dbfc
diyaniliev96 <diyan.iliev96@gmail.com>
2024-05-09 16:40:44 +0300

    TypeScript inheritance training

---
+++ 04-classes/04-inheritance/animal.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Animal = void 0;
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    animalInfo() {
+        return `This animal name is ${this.name} and his age is ${this.age}`;
+    }
+}
+exports.Animal = Animal;
